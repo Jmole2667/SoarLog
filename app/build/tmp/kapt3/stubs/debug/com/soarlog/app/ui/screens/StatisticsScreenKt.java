e1fabd1e13af886a4ee46c3975810612ec00d0ef
Jackm <jack.mole77@gmail.com>
2025-07-28 15:12:07 +0100

    hi

--- app/build/tmp/kapt3/stubs/debug/com/soarlog/app/ui/screens/StatisticsScreenKt.java
+++ app/build/tmp/kapt3/stubs/debug/com/soarlog/app/ui/screens/StatisticsScreenKt.java
@@ -2,7 +2,7 @@ package com.soarlog.app.ui.screens;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000\"\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\u001a\u0016\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003H\u0007\u001a2\u0010\u0005\u001a\u00020\u00012\f\u0010\u0006\u001a\b\u0012\u0004\u0012\u00020\u00070\u00032\u0006\u0010\b\u001a\u00020\u00072\u0012\u0010\t\u001a\u000e\u0012\u0004\u0012\u00020\u0007\u0012\u0004\u0012\u00020\u00010\nH\u0007\u00a8\u0006\u000b"}, d2 = {"StatisticsScreen", "", "flights", "", "Lcom/soarlog/app/models/Flight;", "YearSelector", "years", "", "selectedYear", "onYearSelected", "Lkotlin/Function1;", "app_debug"})
+@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000\"\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\u001a\"\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u0006H\u0007\u001a\u0016\u0010\u0007\u001a\u00020\u00012\f\u0010\b\u001a\b\u0012\u0004\u0012\u00020\n0\tH\u0007\u00a8\u0006\u000b"}, d2 = {"StatBox", "", "title", "", "value", "modifier", "Landroidx/compose/ui/Modifier;", "StatisticsScreen", "flights", "", "Lcom/soarlog/app/models/Flight;", "app_debug"})
 public final class StatisticsScreenKt {
     
     @androidx.compose.runtime.Composable
@@ -12,8 +12,9 @@ public final class StatisticsScreenKt {
     }
     
     @androidx.compose.runtime.Composable
-    public static final void YearSelector(@org.jetbrains.annotations.NotNull
-    java.util.List<java.lang.Integer> years, int selectedYear, @org.jetbrains.annotations.NotNull
-    kotlin.jvm.functions.Function1<? super java.lang.Integer, kotlin.Unit> onYearSelected) {
+    public static final void StatBox(@org.jetbrains.annotations.NotNull
+    java.lang.String title, @org.jetbrains.annotations.NotNull
+    java.lang.String value, @org.jetbrains.annotations.NotNull
+    androidx.compose.ui.Modifier modifier) {
     }
 }
